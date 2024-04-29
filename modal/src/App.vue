6e707934b03a3ac0a40cd33870551ac18d60987d
PROJET WEB <abdelilah.benchine3@gmail.com>
2024-04-29 14:15:11 +0200

    Login/Register page added

--- modal/src/App.vue
+++ modal/src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <login />
 </template>
 
 <script>
-
+import login from './components/Login.vue'
 export default {
   name: 'App',
+  components: {
+    login
+  }
 }
 </script>
 
